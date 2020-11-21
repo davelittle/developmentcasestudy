2b7ba67f45ea8bc60c8ae831d2c9eb28bf3e87ab
davelittle <74601903+davelittle@users.noreply.github.com>
2020-11-21 23:15:28 +0000

    Add files via upload

---
+++ CardPreQualificationTool/Models/Rejection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardPreQualificationTool.Models
+{
+    public class Rejection
+    {
+        public string Reason { get; set; }
+
+        public override string ToString()
+        {
+            return $"Rejected: {Reason}";
+        }
+    }
+}
